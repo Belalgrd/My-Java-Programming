8d13ed51673fee83e4abfc5df48cd3bf94ae2a5f
Belalgrd <iqubalk999@gmail.com>
2025-10-29 14:07:13 +0530

    Method Type

---
+++ JavaCode/JavaPractice/src/MethodsTopic.java
@@ -0,0 +1,19 @@
+
+public class MethodsTopic {
+
+	public static void main(String[] args) {
+		/*
+	    The formula for a triangle is (base * height) / 2
+	    So let's define 2 variables for the needed values and do the calculation below
+	    */
+	    double triangleBase = 3.5;
+	    double triangleHeight = 7.0;
+
+	    double area = triangleBase * triangleHeight;
+	    area = area / 2;
+	    
+	    System.out.println("The area of the triangle is " + area);
+
+	}
+
+}
