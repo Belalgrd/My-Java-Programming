77bdf57b7d992bdbe2c21f841efcf02694f6b349
Belalgrd <iqubalk999@gmail.com>
2025-10-30 10:34:47 +0530

    Increment operator

---
+++ JavaCode/JavaPractice/src/Increment.java
@@ -0,0 +1,15 @@
+
+public class Increment {
+
+	public static void main(String[] args) {
+		int number = 5;
+		System.out.println("Original number: " + number);
+		
+		// Incrementing the number by 1
+		number++;
+		
+		System.out.println("Incremented number: " + number);
+
+	}
+
+}
