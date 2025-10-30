761f90f751f1456889a49d8a06a978f1ceaad9f5
Belalgrd <iqubalk999@gmail.com>
2025-10-30 10:36:15 +0530

    Decrement operator

---
+++ JavaCode/JavaPractice/src/Decrement.java
@@ -0,0 +1,15 @@
+
+public class Decrement {
+
+	public static void main(String[] args) {
+		int number = 5;
+		System.out.println("Original number: " + number);
+		
+		// Decrementing the number by 1
+		number--;
+		
+		System.out.println("Decremented number: " + number);
+
+	}
+
+}
