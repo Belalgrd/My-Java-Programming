350af1879033fa57a5c1e68b43b05fffe13ae3d9
Belalgrd <iqubalk999@gmail.com>
2025-11-11 22:12:48 +0530

    Equality Operator

---
+++ JavaCode/JavaPractice/src/EqualityOperator.java
@@ -0,0 +1,18 @@
+
+public class EqualityOperator {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		// Using equality operator (==)
+		boolean isEqual = (a == b);
+		System.out.println("Is a equal to b? " + isEqual);
+
+		// Using inequality operator (!=)
+		boolean isNotEqual = (a != b);
+		System.out.println("Is a not equal to b? " + isNotEqual);
+
+	}
+
+}
