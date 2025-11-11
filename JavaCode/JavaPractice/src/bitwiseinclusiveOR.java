67c65a59101a5aed2056b5b85f68ddddeba63fc4
Belalgrd <iqubalk999@gmail.com>
2025-11-11 22:11:55 +0530

    bitwise inclusive OR

---
+++ JavaCode/JavaPractice/src/bitwiseinclusiveOR.java
@@ -0,0 +1,14 @@
+
+public class bitwiseinclusiveOR {
+
+	public static void main(String[] args) {
+		int a = 5;  // In binary: 0101
+		int b = 3;  // In binary: 0011
+
+		// Using bitwise inclusive OR operator (|)
+		int orResult = a | b;   // Result: 0111 (7 in decimal)
+		System.out.println("Bitwise Inclusive OR of a and b: " + orResult);
+
+	}
+
+}
