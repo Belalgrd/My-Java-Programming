d4b245c7f40444f816ef0cde5ffe30a22e3f990c
Belalgrd <iqubalk999@gmail.com>
2025-11-11 21:46:59 +0530

    Multiplicative operator

---
+++ JavaCode/JavaPractice/src/MultiplicativeOperator.java
@@ -0,0 +1,22 @@
+
+public class MultiplicativeOperator {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+
+		// Multiplication
+		int multiplication = a * b;
+		System.out.println("Multiplication: " + multiplication);
+
+		// Division
+		int division = a / b;
+		System.out.println("Division: " + division);
+
+		// Modulus
+		int modulus = a % b;
+		System.out.println("Modulus: " + modulus);
+
+	}
+
+}
