5650b46c42b9974848c30e7f8aba1aa20087e648
Belalgrd <iqubalk999@gmail.com>
2025-11-11 21:45:25 +0530

    Additive Operator

---
+++ JavaCode/JavaPractice/src/AdditiveOperator.java
@@ -0,0 +1,18 @@
+
+public class AdditiveOperator {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+
+		// Addition
+		int addition = a + b;
+		System.out.println("Addition: " + addition);
+
+		// Subtraction
+		int subtraction = a - b;
+		System.out.println("Subtraction: " + subtraction);
+
+	}
+
+}
