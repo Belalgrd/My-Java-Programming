d4893c42d30daf29da4e23b074773ed64f0427ed
Belalgrd <iqubalk999@gmail.com>
2025-11-11 22:13:27 +0530

    Logical Or

---
+++ JavaCode/JavaPractice/src/logicalOR.java
@@ -0,0 +1,14 @@
+
+public class logicalOR {
+
+	public static void main(String[] args) {
+		int a = 5;  // In binary: 0101
+		int b = 3;  // In binary: 0011
+
+		// Using logical OR operator (||)
+		boolean orResult = (a > 2) || (b > 5); // true || false = true
+		System.out.println("Logical OR of conditions: " + orResult);
+
+	}
+
+}
